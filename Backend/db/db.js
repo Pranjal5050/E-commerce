ad0e0424c264b5e9a2144e49f6d712eadf58800a
Pranjal Sharma <pranjalsharma9143@gmail.com>
2026-09-03 13:46:12 +0530

    Fix MongoDB connection string

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const db = ()=>{
-    mongoose.connect("mongodb+srv://Pranjal2525:Pranjaldev@2525@cluster0.bcoxwf7.mongodb.net/?appName=we-mart").then(()=>{
+    mongoose.connect("mongodb+srv://Pranjal2525:Pranjaldev%402525@cluster0.bcoxwf7.mongodb.net/?appName=we-mart").then(()=>{
         console.log("Db Connected Successfully");
     }).catch((err)=>{
         console.log("Db Error", err);
